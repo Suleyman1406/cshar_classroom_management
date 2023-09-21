af8dc2723af18645c912a8f1f2b1a91f8776d401
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-09-21 11:38:38 +0400

    core layer compeleted with models

---
+++ ClassroomManagement.Core/Models/Class.cs
@@ -0,0 +1,19 @@
+﻿using ClassroomManagement.Core.Common;
+
+namespace ClassroomManagement.Core.Models
+{
+	public class Class:BaseEntity
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public List<Student> students;
+
+        public Class(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            students = new();
+        }
+    }
+}
+
---
+++ ClassroomManagement.Core/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassroomManagement.Core.Common;
+
+namespace ClassroomManagement.Core.Models
+{
+	public class Student:BaseEntity
+	{
+		public static int count = 1;
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+			Id = count++;
+        }
+	}
+}
+
